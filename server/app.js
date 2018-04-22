4285bb1da8b00304f06d4332853ed5415fef4eac
Willem Houm <willem.houm@gmail.com>
2018-04-22 10:21:29 +0200

    First commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+"use strict"
+//require
+var express = require('express'),
+path = require('path');
+//Populate process.env variable
+require('dotenv').config();
+
+var app = express();
+//Middleware
+app.use(express.static(path.join(__dirname, '/..', 'public')));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+//Routing
+app.get('/', function (req, res){
+    res.sendFile('../public/index.html', {root: __dirname});
+
+    //console.log(req.url);
+});
+
+//Add external routes
+// app.use(require('./sql_routes/sql-routes'));
+// app.use(require('./py_routes/py-routes'));
+
+app.listen(process.env.APP_PORT, function (){
+    console.log('Server listening on  : '+process.env.APP_PORT);
+});
+
